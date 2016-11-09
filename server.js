8a12cae68dd806b2c77dbd1a7615d3724d3e4ae5
turgut <turgut@eftsoftware.com>
2016-11-09 13:03:23 +0200

    [master] Add package.json and index.html

--- server.js
+++ server.js
@@ -1,12 +1,9 @@
-s = require("express");
+var express = require("express");
 var bodyParser = require("body-parser");
 var app = express();
 app.use(express.static(__dirname,{index:"index.html"}));
 
-
 app.get("/", function(httpRequest, httpResponse, next){
-    console.log("turgt");
     httpResponse.end();
 });
 app.listen(3002);
-~                   
